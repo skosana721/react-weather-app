096824cc0bd833a75ee19a37c5a6ca7309e026c1
skosana721 <wonganiskosana@gmail.com>
2021-08-27 15:41:24 +0200

    created that shows the timezone

---
+++ weather/src/component/Location.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useSelector } from "react-redux";
+
+export const Location = () => {
+  const weather = useSelector((state) => state.data);
+  return (
+    <div className="location">
+      <h1 className="location-timezone">{weather.timezone}</h1>
+      <p>icon</p>
+    </div>
+  );
+};

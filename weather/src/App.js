0bee62b430639be4d7f46fcf7ba03e8f0d9ad3de
skosana721 <wonganiskosana@gmail.com>
2021-08-27 15:35:46 +0200

    added windod load which get the longitude and latitude

--- weather/src/App.js
+++ weather/src/App.js
@@ -1,7 +1,32 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import { Location } from "./component/Location";
+import { Temperature } from "./component/Temperature";
+import { useDispatch } from "react-redux";
+import { getWeather } from "./redux/actions/weather";
 
 function App() {
-  return <div className="App"></div>;
+  const dispatch = useDispatch();
+
+  window.addEventListener("load", () => {
+    if (navigator.geolocation) {
+      let lat;
+      let long;
+      navigator.geolocation.getCurrentPosition((position) => {
+        lat = position.coords.latitude;
+        long = position.coords.longitude;
+
+        dispatch(getWeather({ lat, long }));
+      });
+    }
+  });
+
+  return (
+    <div className="App">
+      <Location />
+      <Temperature />
+    </div>
+  );
 }
 
 export default App;
